0a8552ba1e1ced5e960fb1596c7023fdd964add6
tschniik <dalbeyan@students.zhaw.ch>
2022-01-07 10:25:07 +0100

    CrimeController

--- src/main/java/ch/zhaw/projectX/controllers/CrimeController.java
+++ src/main/java/ch/zhaw/projectX/controllers/CrimeController.java
@@ -1,72 +1,24 @@
 package ch.zhaw.projectX.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.zhaw.projectX.entities.Crime;
 import ch.zhaw.projectX.repositories.CrimeRepository;
+import ch.zhaw.projectX.repositories.MainRepository;
 
 @RestController
-public class CrimeController {
+@RequestMapping(value ="/crime") //Indicates that all web requests with /crime are mapped with this controller.
+public class CrimeController extends MainController<Crime>{ //Weshalb <Crime>
 
-	@Autowired
-	private CrimeRepository repository;
-
-	@GetMapping("/crime/all")
-	public ResponseEntity<List<Crime>> findAllCrimes(){
-		List<Crime> result = this.repository.findAll();
-			
-		if (result != null && !result.isEmpty()) {
-			return new ResponseEntity<List<Crime>>(result, HttpStatus.OK);}
-		else {
-			return new ResponseEntity<List<Crime>>(HttpStatus.NOT_FOUND);}
+	private CrimeRepository crimeRepository;
+	
+	public CrimeController (MainRepository<Crime> mainRepository, CrimeRepository crimeRepository) { //Weshalb MainRepository<Crime>? Was hat das mit Crime zu tun un wie hat  das mit dem Konstruktor einen Zusammenhang
+		super(mainRepository); //Nicht verstanden ->Konstruktor Vererbung ansehen
+		this.crimeRepository = crimeRepository;
+	
 	}
 
-	@GetMapping("/crime/{id}")
-	public ResponseEntity<Crime> findCrimeById(@PathVariable("id") int id) {
-		if (this.repository.existsById(id)) {
-            	return new ResponseEntity<> (HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-	}
 	
 	
-	@PostMapping("/crime/create-crime")
-	Crime newCrime(@RequestBody Crime newCrime) {
-	return repository.save(newCrime);
-	}
-	
-	@PutMapping("/crime/crime-details/{id}")
-	public ResponseEntity<Crime> update(@PathVariable("id") int id, Crime newEntity){ // Allenfalls neuen Entitätstyp anotieren
-		  if (this.repository.existsById(id)) {
-	            this.repository.save(newEntity); 
-	            	return new ResponseEntity<> (HttpStatus.OK);
-	        } else {
-	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	        }
-		  }
-
-		  
-	@DeleteMapping("/crime/crime-details/{id}")
-		public ResponseEntity<Crime> delete(@PathVariable("id") int id){
-		if (this.repository.existsById(id)) {
-			this.repository.deleteById(id);
-			return new ResponseEntity<>(HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-
 }
