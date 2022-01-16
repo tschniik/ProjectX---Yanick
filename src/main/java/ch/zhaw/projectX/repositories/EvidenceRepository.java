f070a589a8f2a2ec3184ca9d5795a7339b480948
tschniik <dalbeyan@students.zhaw.ch>
2022-01-16 16:41:24 +0100

    Queries

--- src/main/java/ch/zhaw/projectX/repositories/EvidenceRepository.java
+++ src/main/java/ch/zhaw/projectX/repositories/EvidenceRepository.java
@@ -6,10 +6,5 @@ import ch.zhaw.projectX.entities.Evidence;
 @NoRepositoryBean //Annotation to exclude repository interfaces from being picked up and thus in consequence getting an instance being created. This Repository doesn't need its own methods (CRUD) 
 public interface EvidenceRepository<T extends Evidence> extends MainRepository<Evidence>{
 
-/*	@Query
-	("SELECT * FROM evidence WHERE evidence.evidence_name = ?1;")
-	List<Evidence> findAllEvidencesWithName(String name);
-	*/
-	
 }
 
--- src/main/java/ch/zhaw/projectX/repositories/InvestigationRepository.java
+++ src/main/java/ch/zhaw/projectX/repositories/InvestigationRepository.java
@@ -1,11 +1,13 @@
 package ch.zhaw.projectX.repositories;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import ch.zhaw.projectX.entities.Investigation;
 
 public interface InvestigationRepository<T> extends MainRepository<Investigation>{ 
 
-/*	@Query
+	@Query
 	("SELECT * FROM investigation WHERE investigation.crime_commissioner =?1 OR investigation.police_department = ?1;")
 	List<Investigation> findAllInvestigationsWithCommissionerOrPD(String CommissionerOrPD);
-	*/
+	
 }
